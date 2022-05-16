021956de32e6dfcff282e5d8996991697ad3e606
Sho Ito <i.sho.0628@gmail.com>
2022-05-17 00:28:12 +0900

    Add a solution for practiceA

---
+++ atcoder/abs/practice_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, sum;
+  string s;
+
+  cin >> a >> b >> c >> s;
+
+  sum = a + b + c;
+  cout << sum << " " << s << endl;
+  return 0;
+}

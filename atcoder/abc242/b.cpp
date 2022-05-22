4cc28920a00f088571d989cd0592e4d456b196f7
Sho Ito <i.sho.0628@gmail.com>
2022-05-23 00:07:20 +0900

    Add a solution for abc242_b

---
+++ atcoder/abc242/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  string s;
+  cin >> s;
+
+  sort(s.begin(), s.end());
+
+  cout << s << endl;
+
+  return 0;
+}

fcae3bb9fb8b9ad9e550a0a4933071d1876fcd56
Sho Ito <i.sho.0628@gmail.com>
2022-05-30 00:00:22 +0900

    Add a solution for abc223_a

---
+++ atcoder/abc223/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int x; cin >> x;
+
+  if (x != 0 && x % 100 == 0) cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}

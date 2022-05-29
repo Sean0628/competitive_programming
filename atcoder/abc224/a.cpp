552a6ca27a8ff56eadb54f79884dd39e08c84036
Sho Ito <i.sho.0628@gmail.com>
2022-05-29 23:56:13 +0900

    Add a solution for abc224_a

---
+++ atcoder/abc224/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  string s; cin >> s;
+  if (s[s.size()-1] == 'r') cout << "er" << endl;
+  else cout << "ist" << endl;
+
+  return 0;
+}

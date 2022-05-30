8c4dd2616160f0ae2bbc7639c7133b57cf4705e0
Sho Ito <i.sho.0628@gmail.com>
2022-05-31 00:15:33 +0900

    Add a solution for abc222_a

---
+++ atcoder/abc222/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n; cin >> n;
+  cout << setfill('0') << setw(4) << n << endl;
+
+  return 0;
+}

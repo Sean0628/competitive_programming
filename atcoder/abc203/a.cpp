e12bbaff66605ee1712056389860d49e66103a80
Sho Ito <i.sho.0628@gmail.com>
2022-06-04 16:20:35 +0900

    Add a solution for abc203_a

---
+++ atcoder/abc203/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int a, b, c; cin >> a >> b >> c;
+
+  if (a == b) cout << c << endl;
+  else if (a == c) cout << b << endl;
+  else if (b == c) cout << a << endl;
+  else cout << 0 << endl;
+
+  return 0;
+}

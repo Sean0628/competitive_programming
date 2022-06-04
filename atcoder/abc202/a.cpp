339ca3de31a563882c4a94a10725a069e2739a80
Sho Ito <i.sho.0628@gmail.com>
2022-06-04 16:27:52 +0900

    Add a solution for abc202_a

---
+++ atcoder/abc202/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int a, b, c; cin >> a >> b >> c;
+
+  cout << (7-a) + (7-b) + (7-c) << endl;
+
+  return 0;
+}

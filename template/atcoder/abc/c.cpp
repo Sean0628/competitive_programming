3fe0beebf76ca31bcd4f0c46809b2124358b89a6
Sho Ito <i.sho.0628@gmail.com>
2022-09-18 15:51:36 +0900

    Mod templates

--- template/atcoder/abc/c.cpp
+++ template/atcoder/abc/c.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 using P = pair<int, int>;
@@ -7,3 +9,4 @@ using P = pair<int, int>;
 int main() {
   return 0;
 }
+

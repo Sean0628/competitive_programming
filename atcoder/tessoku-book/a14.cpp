85ced45992d46e1f3321b24632863370344c0ae4
Sho Ito <i.sho.0628@gmail.com>
2022-09-23 18:12:46 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/a14.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n, k; cin >> n >> k;
+  vector<int> a(n), b(n), c(n), d(n);
+  rep(i, n) cin >> a[i];
+  rep(i, n) cin >> b[i];
+  rep(i, n) cin >> c[i];
+  rep(i, n) cin >> d[i];
+
+  vector<int> p(n*n), q(n*n);
+  rep(i, n) rep(j, n) p[i*n+j] = a[i] + b[j];
+  rep(i, n) rep(j, n) q[i*n+j] = c[i] + d[j];
+
+  sort(q.begin(), q.end());
+
+  bool ok = false;
+  rep(i, n*n) {
+    int pos = lower_bound(q.begin(), q.end(), k-p[i]) - q.begin();
+    if (pos < n*n && q[pos] == k-p[i]) ok = true;
+  }
+
+  cout << (ok ? "Yes" : "No") << endl;
+  return 0;
+}

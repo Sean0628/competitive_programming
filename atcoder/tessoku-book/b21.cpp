c68e2a35669390a9e1556e500f6c7f59f38c7a3c
Sho Ito <i.sho.0628@gmail.com>
2022-09-26 18:11:41 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/b21.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  vector dp(n+1, vector<int> (n+1, 0));
+  for (int i = 1; i <= n; ++i) dp[i][i] = 1;
+
+  for (int len = 2; len <= n; ++len) {
+    for (int l = 1; l < n; ++l) {
+      int r = l + len - 1;
+      if (r > n) continue;
+
+      if (s[l-1] == s[r-1]) dp[l][r] = max(dp[l+1][r-1] + 2, dp[l][r]);
+      else dp[l][r] = max(dp[l+1][r], dp[l][r-1]);
+    }
+  }
+
+  cout << endl;
+  cout << dp [1][n] << endl;
+
+  return 0;
+}

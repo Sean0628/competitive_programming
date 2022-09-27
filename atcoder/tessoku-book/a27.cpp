6e3f687ae898b6fe8db6fc78c0a8d18ae30a2b84
Sho Ito <i.sho.0628@gmail.com>
2022-09-27 19:15:36 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/a27.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+ll gcd(ll a, ll b) {
+  if (b == 0) return a;
+
+  return gcd(b, a%b);
+}
+
+int main() {
+  ll a, b; cin >> a >> b;
+  cout << gcd(a, b) << endl;
+  return 0;
+}

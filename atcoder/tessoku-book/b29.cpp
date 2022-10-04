5bdd08413830fff0918d4b0a935bf1348e78df73
Sho Ito <i.sho.0628@gmail.com>
2022-10-04 11:20:08 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/b29.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+const int MOD = 1e9 + 7;
+
+ll powmod(ll x, ll y, ll mod) {
+  ll p = x, ans = 1;
+
+  rep(i, 60) {
+    ll n = (1LL << i);
+    if ((y/n) % 2 == 1) ans = (ans*p) % mod;
+
+    p = (p*p) % mod;
+  }
+
+  return ans;
+}
+
+int main() {
+  ll a, b; cin >> a >> b;
+  cout << powmod(a, b, MOD) << endl;
+  return 0;
+}

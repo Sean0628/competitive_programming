6ed61699a9536e6ca5c4c4d455de3f9d0e217619
Sho Ito <i.sho.0628@gmail.com>
2022-11-05 19:21:08 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/a58.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+class SegmentTree {
+  public:
+    vector<int> dat;
+    int siz = 1;
+
+  void init(int n) {
+    siz = 1;
+    while (siz < n) siz *= 2;
+    for (int i = 1; i < siz * 2; ++i) dat.emplace_back(0);
+  }
+
+  void update(int pos, int x) {
+    pos = pos + siz - 1;
+    dat[pos] = x;
+    while (pos >= 2) {
+      pos /= 2;
+      dat[pos] = max(dat[pos*2], dat[pos*2+1]);
+    }
+  }
+
+  int query(int l, int r, int a, int b, int u) {
+    if (r <= a || b <= l) return INT_MIN;
+    if (l <= a && b <= r) return dat[u];
+
+    int m = (a + b) / 2;
+    int ansl = query(l, r, a, m, u*2);
+    int ansr = query(l, r, m, b, u*2+1);
+
+    return max(ansl, ansr);
+  }
+};
+
+int main() {
+  int n, q; cin >> n >> q;
+
+  SegmentTree st;
+  st.init(n);
+  rep(i, q) {
+    int c, pos, x, l, r;
+    cin >> c;
+    if (c == 1) {
+      cin >> pos >> x;
+      st.update(pos, x);
+    } else {
+      cin >> l >> r;
+      int ans = st.query(l, r, 1, st.siz + 1, 1);
+
+      cout << ans << endl;
+    }
+  }
+  return 0;
+}

8323690f3bf07c641311fe03001c64252f72ddab
Sho Ito <i.sho.0628@gmail.com>
2022-11-13 14:58:16 +0900

    Add solutions for tessoku-book

---
+++ atcoder/tessoku-book/a73.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  ll n, m; cin >> n >> m;
+
+  vector<vector<P>> to(n+1);
+  rep(i, m) {
+    ll a, b, c ,d; cin >> a >> b >> c >> d;
+    if (d == 1) {
+      to[a].emplace_back(make_pair(b, 10000LL*c - 1LL));
+      to[b].emplace_back(make_pair(a, 10000LL*c - 1LL));
+    } else {
+      to[a].emplace_back(make_pair(b, 10000LL*c));
+      to[b].emplace_back(make_pair(a, 10000LL*c));
+    }
+  }
+
+  vector<bool> fixed(n+1);
+  vector<ll> cur(n+1, LONG_MAX);
+  priority_queue<P, vector<P>, greater<P>> que;
+
+  cur[1] = 0;
+  que.push(make_pair(cur[1], 1));
+
+  // dijkstra's
+  while (!que.empty()) {
+    int pos = que.top().second; que.pop();
+    if (fixed[pos]) continue;
+
+    fixed[pos] = true;
+
+    rep(i, to[pos].size()) {
+      int next = to[pos][i].first;
+      int cost = to[pos][i].second;
+
+      if (cur[next] > cur[pos] + cost) {
+        cur[next] = cur[pos] + cost;
+        que.push(make_pair(cur[next], next));
+      }
+    }
+  }
+
+  int dist = (cur[n] + 9999) / 10000;
+  int t = dist * 10000 - cur[n];
+
+  cout << dist << " " << t << endl;
+  return 0;
+}
---
+++ atcoder/tessoku-book/a74.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n; cin >> n;
+  vector p(n, vector<int> (n));
+  vector<int> x(n), y(n);
+  rep(i, n) rep(j, n) {
+    cin >> p[i][j];
+    if (p[i][j] != 0) { x[i] = p[i][j]; y[j] = p[i][j]; }
+  }
+
+  int inversionX = 0, inversionY = 0;
+  rep(i, n) {
+    for (int j = i+1; j < n; ++j) {
+      if (x[i] > x[j]) inversionX += 1;
+      if (y[i] > y[j]) inversionY += 1;
+    }
+  }
+
+  cout << inversionX + inversionY << endl;
+  return 0;
+}

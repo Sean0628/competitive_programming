4c9d362e33b37e26ee766b885abb26c3a8b30af7
Sho Ito <i.sho.0628@gmail.com>
2022-12-28 08:48:34 +0900

    Add a solution for abc282

---
+++ atcoder/abc282/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  vector<char> v = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+  int k; cin >> k;
+  string ans = "";
+  rep(i, k) {
+    ans += v[i];
+  }
+  cout << ans << endl;
+
+  return 0;
+}
+

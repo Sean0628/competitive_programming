2d8087d3e1657b2f0ef91ddd4aa91276fa3b2a2e
Sho Ito <i.sho.0628@gmail.com>
2023-03-25 17:22:52 +0900

    Add a solution for abc082

---
+++ atcoder/abc/abc082/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  map<int, ll> mp;
+  rep(i, n) mp[a[i]]++;
+
+  ll ans = 0;
+  for (auto p : mp) {
+    if (p.first != p.second) {
+      if (p.first < p.second) {
+        ans += (p.second - p.first);
+      } else {
+        ans += p.second;
+      }
+    }
+  }
+
+  out(ans);
+  return 0;
+}

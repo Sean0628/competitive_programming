fe5d80c1a59754850afbf027a6173a4445985032
Sho Ito <i.sho.0628@gmail.com>
2023-04-08 21:20:02 +0900

    Add a solution for abc059

---
+++ atcoder/abc/abc059/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  string a, b, c; cin >> a >> b >> c;
+  string ans = "";
+  ans += toupper(a[0]);
+  ans += toupper(b[0]);
+  ans += toupper(c[0]);
+
+  out(ans);
+  return 0;
+}

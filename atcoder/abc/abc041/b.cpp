7c2ce9939fa52ab9b2fec531ad2cf7ad9f391a7d
Sho Ito <i.sho.0628@gmail.com>
2023-04-11 17:22:58 +0900

    Add a solution for abc041

---
+++ atcoder/abc/abc041/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  ll a, b, c; cin >> a >> b >> c;
+
+  mint ans = a;
+  ans *= b;
+  ans *= c;
+
+  out(ans.val());
+  return 0;
+}

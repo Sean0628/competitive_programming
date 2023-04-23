bb1e05abcaada7e980d1609c8cf7a4a8f40f27c7
Sho Ito <i.sho.0628@gmail.com>
2023-04-23 18:46:49 +0900

    Add a solution for abc020

---
+++ atcoder/abc/abc020/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  int q; cin >> q;
+  if (q == 1) {
+    out("ABC");
+  } else {
+    out("chokudai");
+  }
+  return 0;
+}

5cf916c9e57f96806cf4073b0f7def06a0c9b3f2
Sho Ito <i.sho.0628@gmail.com>
2023-04-25 13:17:56 +0900

    Add a solution for abc009

---
+++ atcoder/abc/abc009/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+
+  sort(a.rbegin(), a.rend());
+  a.erase(unique(a.begin(), a.end()), a.end());
+
+  out(a[1]);
+
+  return 0;
+}

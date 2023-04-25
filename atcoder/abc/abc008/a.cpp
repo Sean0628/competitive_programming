22dd3e43bc2d349da33ed7d597af9dfbe8bf6f53
Sho Ito <i.sho.0628@gmail.com>
2023-04-25 13:23:02 +0900

    Add a solution for abc008

--- atcoder/abc/abc008/a.cpp
+++ atcoder/abc/abc008/a.cpp
@@ -8,11 +8,11 @@ using ll = long long;
 using P = pair<int, int>;
 using mint = modint1000000007;
 void chmax(ll& a, ll b) { a = max(a, b); }
-void chmax(int& a, int b) { a = max(a, b); }
 void chmin(ll& a, ll b) { a = min(a, b); }
-void chmin(int& a, int b) { a = min(a, b); }
 void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
 
 int main() {
+  int s, t; cin >> s >> t;
+  out(t-s+1);
   return 0;
 }
--- atcoder/abc/abc008/b.cpp
+++ atcoder/abc/abc008/b.cpp
@@ -14,5 +14,24 @@ void chmin(int& a, int b) { a = min(a, b); }
 void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
 
 int main() {
+  int n; cin >> n;
+  vector<string> a(n);
+  rep(i, n) cin >> a[i];
+  map<string, int> mp;
+
+  rep(i, n) mp[a[i]]++;
+
+  int mx = 0;
+  for (auto p : mp) {
+    chmax(mx, p.second);
+  }
+
+  for (auto p : mp) {
+    if (p.second == mx) {
+      out(p.first);
+      return 0;
+    }
+  }
+
   return 0;
 }

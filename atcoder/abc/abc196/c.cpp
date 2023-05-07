cd30d4506cce024e057c59578f523eb768ba0019
Sho Ito <i.sho.0628@gmail.com>
2023-05-07 17:37:28 +0900

    Add a solution for abc196

---
+++ atcoder/abc/abc196/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<double, double>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmax(int& a, int b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void chmin(int& a, int b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  ll n; cin >> n;
+
+  int idx = 1;
+  while (stoll(to_string(idx) + to_string(idx)) <= n) {
+    idx++;
+  }
+
+  out(idx - 1);
+  return 0;
+}

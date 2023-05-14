88437d9327993daeb047d1cdf64b7867fe84cd3d
Sho Ito <i.sho.0628@gmail.com>
2023-05-14 19:38:50 +0900

    Add a solution for abc187

--- atcoder/abc/abc187/d.cpp
+++ atcoder/abc/abc187/d.cpp
@@ -5,7 +5,7 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define out(arg) cout << (arg) << endl
 using ll = long long;
-using P = pair<ll, ll>;
+using P = pair<int, int>;
 using mint = modint1000000007;
 void chmax(ll& a, ll b) { a = max(a, b); }
 void chmax(int& a, int b) { a = max(a, b); }
@@ -14,22 +14,32 @@ void chmin(int& a, int b) { a = min(a, b); }
 void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
 
 int main() {
-  int n; cin >> n;
-  ll tot = 0;
-  vector<ll> a(n);
-  rep(i, n) {
-    ll x, y; cin >> x >> y;
-    tot -= x;
-    a[i] = x + x + y;
-  }
+  int n, m; cin >> n >> m;
+  vector<int> a(m+2, 0);
+  rep(i, m) cin >> a[i+1];
+  a[m+1] = n+1;
 
   sort(a.begin(), a.end());
-  ll ans = 0;
-  while (tot <= 0) {
-    tot += a.back();
-    a.pop_back();
-    ans++;
+
+  vector<int> w;
+  int mn = 1e9;
+  for (int i = 1; i <= m+1; ++i) {
+    int d = a[i] - a[i-1] - 1;
+    if (d == 0) continue;
+    chmin(mn, d);
+    w.push_back(d);
   }
+
+  if (n == m) {
+    out(0);
+    return 0;
+  }
+
+  int ans = 0;
+  int sz = w.size();
+  rep(i, sz) ans += (w[i]+mn-1) / mn;
+
+  if (sz == 0) ans = 1;
 
   out(ans);
 

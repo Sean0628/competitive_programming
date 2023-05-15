2809b12ab84fc0f3bdf4529f91c9780f382649b3
Sho Ito <i.sho.0628@gmail.com>
2023-05-15 18:01:00 +0900

    Add a solution for abc173

---
+++ atcoder/abc/abc173/d.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmax(int& a, int b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void chmin(int& a, int b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+
+  sort(a.rbegin(), a.rend());
+
+  ll ans = 0;
+  for (int i = 1; i < n; i++) {
+    ans += a[i/2];
+  }
+
+  out(ans);
+
+  return 0;
+}

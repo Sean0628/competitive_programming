29b6fcfa1cb61a9990f05603680c64b3e90207ae
Sho Ito <i.sho.0628@gmail.com>
2023-06-03 19:46:34 +0900

    Add a solution for tessoku-book

---
+++ atcoder/tessoku-book/3/b36.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmax(int& a, int b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void chmin(int& a, int b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int main() {
+  int n, k; cin >> n >> k;
+  string s; cin >> s;
+  int cnt = 0;
+  rep(i, n) if (s[i] == '1') cnt++;
+
+  bool ok = false;
+
+  if (cnt%2 == k%2) ok = true;
+
+  cyn(ok);
+
+  return 0;
+}

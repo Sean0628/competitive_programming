9f6e8a95c9dabe3d166d2dc409ae4000bdbc92d7
Sho Ito <i.sho.0628@gmail.com>
2023-06-06 09:28:04 +0900

    Add a solution for tessoku-book

---
+++ atcoder/tessoku-book/3/a59.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define out(arg) cout << (arg) << endl
+using ll = long long;
+using P = pair<int, int>;
+using mint = modint1000000007;
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmax(int& a, int b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void chmin(int& a, int b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+int op(int a, int b) { return a + b; }
+int e() { return 0; }
+
+int main() {
+  int n, q; cin >> n >> q;
+  segtree<int, op, e> seg(n);
+
+  rep(i, q) {
+    int c; cin >> c;
+    if (c == 1) {
+      int x, v; cin >> x >> v;
+      x--;
+      seg.set(x, v);
+    } else {
+      int l, r; cin >> l >> r;
+      l--, r--;
+      out(seg.prod(l, r));
+    }
+  }
+
+  return 0;
+}

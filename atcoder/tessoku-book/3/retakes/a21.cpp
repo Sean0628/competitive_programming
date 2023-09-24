1690f81fc44ba15d0073ddef7ca780aa982eb6e2
Sho Ito <i.sho.0628@gmail.com>
2023-09-24 17:23:22 +0900

    Add solutions for retakes

--- atcoder/tessoku-book/3/retakes/a21.cpp
+++ atcoder/tessoku-book/3/retakes/a21.cpp
@@ -22,5 +22,33 @@ void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
 const int INF = 1e9;
 
 int main() {
+  int n; cin >> n;
+  vector<int> p(n), a(n);
+  rep(i, n) {
+    cin >> p[i] >> a[i];
+    p[i]--;
+  }
+
+  // dp[i][j] := 残り [i, j] の状態から始めたときの最大値
+  vector<vector<int>> dp(n, vector<int>(n, 0));
+  dp[0][n-1] = 0;
+
+  for (int len = n - 2; len >= 0; --len) {
+    for (int l = 0; l + len < n; ++l) {
+      int r = l + len;
+
+      int s1 = (l <= p[l-1] && p[l-1] <= r) ? a[l-1] : 0;
+      int s2 = (l <= p[r+1] && p[r+1] <= r) ? a[r+1] : 0;
+
+      if (l == 0) dp[l][r] = dp[l][r+1] + s2;
+      else if (r == n-1) dp[l][r] = dp[l-1][r] + s1;
+      else dp[l][r] = max(dp[l-1][r] + s1, dp[l][r+1] + s2);
+    }
+  }
+
+  ll ans = 0;
+  rep(i, n) chmax(ans, dp[i][i]);
+
+  out(ans);
   return 0;
 }

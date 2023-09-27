0088914496ce90523a0e918302c2f6da39d4d26c
Sho Ito <i.sho.0628@gmail.com>
2023-09-27 23:33:15 +0900

    Add solutions for kakomon100

---
+++ misc/kako100/53.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define out(arg) cout << (arg) << endl
+#define debug(x) cerr << #x << ": " << (x) << endl
+#define debugv(x) cerr << #x << ": " << endl; for (auto& y : x) cerr << y << " "; cerr << endl;
+#define debugvv(x) cerr << #x << ": " << endl; for (auto& y : x) { for (auto& z : y) cerr << z << " "; cerr << endl; }
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+using ll = long long;
+
+void chmax(ll& a, ll b) { a = max(a, b); }
+void chmax(int& a, int b) { a = max(a, b); }
+void chmin(ll& a, ll b) { a = min(a, b); }
+void chmin(int& a, int b) { a = min(a, b); }
+void cyn(bool x) { cout << (x ? "Yes" : "No") << endl; }
+
+const int INF = 1e9;
+
+int main() {
+  int n; cin >> n;
+  vector<int> a(n); rep(i, n) cin >> a[i];
+
+  int len = 0;
+  vector<int> l(n, INF);
+
+  // LIS
+  rep(i, n) {
+    int pos = lower_bound(l.begin(), l.end(), a[i]) - l.begin();
+
+    l[pos] = a[i];
+    chmax(len, pos + 1);
+  }
+
+  out(len);
+  return 0;
+}
+
